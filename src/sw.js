b10dcf75d58cc0aa159834d4781ec6b550bccd19
Unknown <hallo@maximilianberndt.com>
2019-09-11 10:16:38 +0200

    ADDED: noscript tag

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Names of the cache used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'v4';
+const PRECACHE = 'v1';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
